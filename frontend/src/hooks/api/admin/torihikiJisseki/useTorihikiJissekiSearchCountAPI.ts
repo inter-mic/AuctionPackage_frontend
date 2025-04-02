7a8fe79cd3f2e96f8654c808df1f58fbe920039c
mic-namba <72436517+mic-namba@users.noreply.github.com>
2025-04-02 10:16:56 +0900

    取引実績カウントAPI修正

--- frontend/src/hooks/api/admin/torihikiJisseki/useTorihikiJissekiSearchCountAPI.ts
+++ frontend/src/hooks/api/admin/torihikiJisseki/useTorihikiJissekiSearchCountAPI.ts
@@ -7,7 +7,7 @@ export const useTorihikiJissekiSearchCountAPI = () => {
   const { useState, useEffect, useCallback, useRouter, texts, apiRequest } = useCommonSetup();
   const [count, setCount] = useState<number | 0>(0);
   const torihikiJissekiSearchCountAPI = async (searchParams: TMemberTorihikiJissekiRequest) => {
-    const { status, data: responseData } = await apiRequest("member", 'torihikiJisseki/count', 'POST', searchParams, "", true);
+    const { status, data: responseData } = await apiRequest("admin", 'torihikiJisseki/count', 'POST', searchParams, "", true);
     if (status == 200 && responseData) {
       setCount(responseData);
     }
