5b1108c56e61f207c46318dd4db02d89e4036bfd
mic-namba <72436517+mic-namba@users.noreply.github.com>
2025-04-04 14:07:11 +0900

    _app画面修正

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import 'dayjs/locale/zh-cn';
@@ -10,6 +11,9 @@ import '@/styles/globals.css';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="zh-cn">
+      <Head>
+        <title></title>
+      </Head>
      <Component {...pageProps} />
     </LocalizationProvider>
   );
