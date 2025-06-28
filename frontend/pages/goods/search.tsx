5588b83fc490e8994379514c42fc35f3bb9098f2
mic-namba <72436517+mic-namba@users.noreply.github.com>
2025-06-28 13:20:17 +0900

    脆弱性のエラー修正

--- frontend/pages/goods/search.tsx
+++ frontend/pages/goods/search.tsx
@@ -6,8 +6,19 @@ import withMemberisLoginLayout from "@/hocs/withMemberisLoginLayout";
 
 export const getServerSideProps: GetServerSideProps = withSystemSetting(
   async (context) => {
-    const { locale } = context;
+    const { query, locale } = context;
+
+    const auctionSeq = query.auctionSeq;
     const texts = getTexts(locale);
+
+    if (!auctionSeq) {
+      return {
+        redirect: {
+          destination: "/login", // または別のURL
+          permanent: false, // 302 リダイレクト（true にすると 301）
+        },
+      };
+    }
     return {
       props: {
         pageTitle: texts.menu.memberGoodsList,
